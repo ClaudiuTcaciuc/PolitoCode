99670fa14c20f1259f2b0066f4845586d55c6509
Claudiu Tcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2023-04-13 16:40:22 +0200

    MLPT lab05 init

---
+++ pyCode/lab05/lab05.py
@@ -0,0 +1,57 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+#load the data with sklearn
+def load_iris():
+    data, label = datasets.load_iris()['data'].T, datasets.load_iris()['target']
+    return data, label
+
+def split_data(data, label, seed=0):
+    n_train = int(data.shape[1] * 2.0/3.0)
+    np.random.seed(seed)
+    index = np.random.permutation(data.shape[1])
+    index_train = index[:n_train]
+    index_test = index[n_train:]
+
+    data_train = data[:, index_train]
+    label_train = label[index_train]
+    data_test = data[:, index_test]
+    label_test = label[index_test]
+
+    return (data_train, label_train), (data_test, label_test)
+
+def multivariate_gaussian_classifier(data_train, label_train, data_test, label_test):
+    # compute the mean and the covariance matrix for each class
+    score = np.zeros((3, data_test.shape[1]))
+    print(score.shape)
+    for i in np.unique(label_train):
+        mean = np.mean(data_train[:, label_train == i], axis=1)
+        cov = np.cov(data_train[:, label_train == i])
+    
+    for i in np.unique(label_train):
+        print("mean of class", i, "is\n", mean)
+        print("covariance matrix of class", i, "is\n", cov)
+
+    
+
+def log_likelihood(x, mean, cov):
+    # compute the log likelihood of x given the multivariate Gaussian distribution with mean and cov
+    return log_pdf_multivariate_gaussian(x, mean, cov)
+
+def log_pdf_multivariate_gaussian(x, mean, sigma):
+    # compute the log pdf of the multivariate Gaussian distribution with mean and sigma
+    size = len(x)
+    sign, sigma_det = np.linalg.slogdet(sigma)
+    sigma_inv = np.linalg.inv(sigma)
+    centered_data = x - mean.reshape(-1, 1)
+    
+    term_1 = -size/2 * np.log(2*np.pi)
+    term_2 = -1/2 * sign * sigma_det
+    term_3 = -1/2 * np.sum(centered_data.T.dot(sigma_inv) * centered_data.T, axis=1)
+    return term_1 + term_2 + term_3
+
+if __name__ == "__main__":
+    data, label = load_iris()
+    (data_train, label_train), (data_test, label_test) = split_data(data, label)
+    multivariate_gaussian_classifier(data_train, label_train, data_test, label_test)
