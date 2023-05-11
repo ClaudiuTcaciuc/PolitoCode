df342d57f33b2af1181a61ed2de5023f81fc30fc
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2023-05-11 12:26:46 +0200

    some progress

--- pyCode/lab07/lab_07.py
+++ pyCode/lab07/lab_07.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.optimize as opt
 from sklearn import datasets
+#test
+from sklearn.metrics import accuracy_score
 
 def load_iris_binary():
     data, label = datasets.load_iris()['data'].T, datasets.load_iris()['target']
@@ -37,12 +39,42 @@ def gradient_approximation(x, h=1e-7):
         gradient_approx.append((function_to_solve(x_plus_h) - function_to_solve(x))/h)
     return gradient_approx
 
-#def logreg_obj (x, data, label, lambda_):
+def sigmoid (z):
+    return 1/(1 + np.exp(-z))
+
+def logreg_obj (v, data, label, lambda_):
+    d, n = data.shape
+    v = v.reshape((d+1, 1))
+    data = np.vstack((np.ones((1, n)), data))
+    z = np.dot(v.T, data)
+    J = lambda_/2 * np.dot(v.T, v) + (1.0/n)*np.sum(np.logaddexp(0, -label*z))
+    return J
+
+def logreg_obj_v2(v, data, label, lambda_):
+    d, n = data.shape
+    w, b = v[0:-1].reshape((d,1)), v[-1]
+    z = np.dot(w.T, data) + b
+    loss = np.logaddexp(0, -label * z)
+    reg = lambda_ / 2 * np.dot(w.T, w)
+    J = np.sum(loss) / n + reg
+    return J
 
 if __name__ == '__main__':
-    x0 = [0, 0]
-    res_with_approx_grad = opt.fmin_l_bfgs_b(function_to_solve, x0, approx_grad=True)
-    res_with_calculated_grad = opt.fmin_l_bfgs_b(function_to_solve, x0, approx_grad=False, fprime=gradient_to_solve)
-    
+    # initial guess
+    x0_test = [0, 0]
+    res_with_approx_grad = opt.fmin_l_bfgs_b(function_to_solve, x0_test, approx_grad=True)
+    res_with_calculated_grad = opt.fmin_l_bfgs_b(function_to_solve, x0_test, approx_grad=False, fprime=gradient_to_solve)
+    #print(res_with_approx_grad)
+    # load data
     data, label = load_iris_binary()
     (data_train, label_train), (data_test, label_test) = split_data_2to1(data, label)
+    data_train_centered = (data_train - np.mean(data_train, axis=1).reshape(-1, 1))/np.std(data_train, axis=1).reshape(-1, 1)
+    # test gradient approximation
+    x0_iris = np.zeros(data_train.shape[0] + 1)
+    res_iris = opt.fmin_l_bfgs_b(logreg_obj_v2, x0_iris, args=(data_train_centered, label_train, 1), approx_grad=True, iprint=0)
+    print(f"res_iris with approx_grad:\n{res_iris}")
+    
+    res_test = logreg_obj_v2(np.ones(2+1), np.array([[1, 2], [3, 4]]), np.array([1, -1]), 0.1)
+    print(f"res_test:\n{res_test}")
+    
+    #https://towardsdatascience.com/logistic-regression-from-scratch-in-python-ec66603592e2
