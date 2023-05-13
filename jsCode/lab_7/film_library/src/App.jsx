4948fa6d1f28c8f24e24bb9a93385a94c2f00720
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2023-05-13 23:26:19 +0200

    finished raw

--- jsCode/lab_7/film_library/src/App.jsx
+++ jsCode/lab_7/film_library/src/App.jsx
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ListGroup, Container, Button, Form, Table, Navbar, Modal } from 'react-bootstrap';
+import { ListGroup, Container, Button, Form, Table, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate, useParams } from 'react-router-dom';
 import './css/style.css';
 
 class film {
@@ -86,7 +86,7 @@ function My_Header() {
           />
         </Form>
         <Navbar.Brand href="#home">
-          <svg className="bi bi-people-circle" width="20" height="20" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <svg className="bi bi-people-circle" width="20" height="20" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z" />
             <path fillRule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
             <path fillRule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clipRule="evenodd" />
@@ -146,16 +146,71 @@ function filterUnseen() {
   return film_lib;
 }
 
-function Populate_TablefromLibrary(props) {
-  const e = props.e;
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+function UpdateFilmForm(props) {
+  const { id } = useParams();
+  const e = film_library.films.find((e) => e.ID_film === parseInt(id));
+  const navigate = useNavigate();
   const [title, setTitle] = useState(e.title);
   const [date, setDate] = useState(dayjs(e.date_watch).format("YYYY-MM-DD"));
-  const [hover, setHover] = useState(0);
   const [formFav, setFormFav] = useState(e.favorite);
+  let rate = e.rating;
+  const setRating = (value) => {
+    e.rating = value;
+    props.setUpdateFilm(new film(e.ID_film, e.title, e.favorite, e.date_watch, value))
+  }
+  let favorite = e.favorite;
+  const setFavorite = (value) => {
+    e.favorite = value;
+    props.setUpdateFilm(new film(e.ID_film, e.title, value, e.date_watch, e.rating))
+  };
+
+  const handleResetClose = () => {
+    setTitle(e.title);
+    setDate(dayjs(e.date_watch).format("YYYY-MM-DD"));
+    setRating(parseInt(e.rating));
+    setFavorite(e.favorite);
+    navigate(-1);
+  }
+
+  const handleModifyFilm = (event) => {
+    event.preventDefault();
+    if (title != "") {
+      let new_film = new film(
+        e.ID_film,
+        title,
+        formFav,
+        dayjs(date),
+        rate,
+      );
+      props.setUpdateFilm(new_film);
+      navigate(-1);
+    }
+  }
+  return (
+    <Container fluid>
+      <h1 className="mb-2" id="filter-title">Modify Film</h1>
+        <Form onSubmit={handleModifyFilm}>
+          <Form.Group>
+            <Form.Label>Film Title</Form.Label>
+            <Form.Control type="text" placeholder="Film Title" value={title} onChange={ev => setTitle(ev.target.value)}/>
+            <Form.Label>Watch Date</Form.Label>
+            <Form.Control type="date" value={date} onChange={ev => setDate(ev.target.value)}/>
+            <Form.Label>Rating</Form.Label>
+            <Form.Control type="number" label="Rating" min={0} max={5} placeholder="min = 0 max = 5" onChange={ev => setRating(ev.target.value)}/>
+            <Form.Check type="checkbox" label="Favorite" value={favorite} onChange={ev => setFavorite(ev.target.checked)}/>
+          </Form.Group>
+          <Form.Group>
+            <Button variant="primary" type="submit">Modify Film</Button> {' '}
+            <Button variant="secondary" onClick={handleResetClose}>Close</Button>
+          </Form.Group>
+        </Form>
+    </Container>
+  )
+}
+
+function Populate_TablefromLibrary(props) {
+  const e = props.e;
+  const [hover, setHover] = useState(0);
 
   let rate = e.rating;
   const setRating = (value) => {
@@ -172,30 +227,6 @@ function Populate_TablefromLibrary(props) {
     props.setDeleteFilm(e.ID_film);
     film_library.deleteFilm(e.ID_film);
   }
-
-  const handleResetClose = () => {
-    setShow(false);
-    setTitle(e.title);
-    setDate(dayjs(e.date_watch).format("YYYY-MM-DD"));
-    setRating(parseInt(e.rating));
-    setFavorite(e.favorite);
-  }
-
-  const handleModifyFilm = (event) => {
-    event.preventDefault();
-    if (title != "") {
-      handleClose();
-      let new_film = new film(
-        e.ID_film,
-        title,
-        formFav,
-        dayjs(date),
-        rate,
-      );
-      props.setUpdateFilm(new_film);
-    }
-  }
-
   const favorite_checkbox = <Form.Check type="checkbox" label="Favorite" checked={favorite} onChange={() => setFavorite(!favorite)} />;
   const Set_rating_box = () => {
     let rating_box = [];
@@ -232,33 +263,14 @@ function Populate_TablefromLibrary(props) {
       </td>
       <td>{
         <Container fluid >
-          <Button variant="primary" onClick={handleShow} size="sm">
-            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="25" fill="currentColor" className="bi bi-gear-fill" viewBox="0 0 16 16">
-              <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
-            </svg>
-          </Button>
-          <Modal show={show} onHide={handleClose} centered>
-            <Modal.Header closeButton>
-              <Modal.Title>Modify film</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={handleModifyFilm}>
-                <Form.Group>
-                  <Form.Label>Film Title</Form.Label>
-                  <Form.Control type="text" value={title} onChange={ev => setTitle(ev.target.value)} />
-                  <Form.Label>Watch Date</Form.Label>
-                  <Form.Control type="date" value={date} onChange={ev => setDate(ev.target.value)} />
-                  <Form.Label>Rating</Form.Label>
-                  <Form.Control type="number" label="Rating" min={0} max={5} value={rate} onChange={ev => setRating(ev.target.value)} />
-                  <Form.Check type="checkbox" label="Favorite" defaultChecked={formFav} onChange={ev => setFormFav(ev.target.checked)} />
-                </Form.Group>
-                <Form.Group >
-                  <Button variant="secondary" onClick={handleResetClose} size="sm"> Close </Button>{' '}
-                  <Button variant="primary" type="submit" > Modify </Button>
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-          </Modal>{' '}
+          <Link to={`/updatefilm/${e.ID_film}`}>
+            <Button variant="primary" size="sm">
+              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="25" fill="currentColor" className="bi bi-gear-fill" viewBox="0 0 16 16">
+                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+              </svg>
+            </Button>
+          </Link>
+          {' '}
           <Button variant="danger" onClick={() => setDelete()} size="sm">
             <svg xmlns="http://www.w3.org/2000/svg" width="13" height="25" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
@@ -348,7 +360,7 @@ function My_Table(props) {
   );
 }
 
-function My_Footer(props){
+function AddNewFilm (props){
   const navigate = useNavigate();
 
   const [title, setTitle] = useState("");
@@ -366,6 +378,7 @@ function My_Footer(props){
         dayjs(date),
         rating,
       )
+      console.log(new_film);
       props.new_film(() => film_library.populateLibrary(new_film));
       navigate(-1);
     }
@@ -381,15 +394,6 @@ function My_Footer(props){
 
   return (
     <Container fluid>
-      <div className='fixed-bottom-right'>
-        <Link to='/addnewfilm'>
-          <Button variant="primary">
-          <svg xmlns="http://www.w3.org/2000/svg" width="13" height="25" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-          </svg>
-          </Button>
-        </Link>
-      </div>
       <h1 className="mb-2" id="filter-title">Add New Film</h1>
         <Form onSubmit={handleAddFilm}>
           <Form.Group>
@@ -399,13 +403,30 @@ function My_Footer(props){
             <Form.Control type="date" value={date} onChange={ev => setDate(ev.target.value)}/>
             <Form.Label>Rating</Form.Label>
             <Form.Control type="number" label="Rating" min={0} max={5} placeholder="min = 0 max = 5" onChange={ev => setRating(ev.target.value)}/>
-            <Form.Check type="checkbox" label="Favorite" value={favorite} onChange={ev => setFavorite(ev.target.value)}/>
+            <Form.Check type="checkbox" label="Favorite" value={favorite} onChange={ev => setFavorite(ev.target.checked)}/>
           </Form.Group>
           <Form.Group>
             <Button variant="primary" type="submit">Add New Film</Button> {' '}
             <Button variant="secondary" onClick={handleClose}>Close</Button>
           </Form.Group>
         </Form>
+    </Container>    
+  )
+}
+
+function My_Footer(){
+
+  return (
+    <Container fluid>
+      <div className='fixed-bottom-right'>
+        <Link to="addnewfilm">
+          <Button variant="primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="13" height="25" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+          </svg>
+          </Button>
+        </Link>
+      </div>
     </Container>
   );
 }
@@ -414,11 +435,14 @@ function My_Main(props) {
   if (props.new_film_from_form != null)
     props.new_films_to_lib_from_form(() => null);
   return (
+    <>
     <div style={{ display: 'flex', flexDirection: 'row', height: '100vh', overflow: 'scroll initial' }}>
       <div style={{ flex: '1 1 300px' }}>
         <My_Table use_filter={props.use_filter} update_film={props.update_film} setUpdateFilm={props.setUpdateFilm} />
       </div>
     </div>
+    <My_Footer />
+    </>
   );
 }
 
@@ -451,12 +475,11 @@ function App() {
               <Route path="/bestRated" element={<My_Main {...mainProps} use_filter="bestRated" />} />
               <Route path="/seenLastMonth" element={<My_Main {...mainProps} use_filter="seenLastMonth" />} />
               <Route path="/Unseen" element={<My_Main {...mainProps} use_filter="Unseen" />} />
-              <Route path="/updatefilm" element={<My_Main {...mainProps}/>} />
-              <Route path="/addnewfilm" element={<My_Footer new_film={addNewFilm} />} />
+              <Route path="/updatefilm/:id" element={<UpdateFilmForm {...mainProps}/>} />
+              <Route path="/addnewfilm" element={<AddNewFilm new_film={addNewFilm} />} />
             </Routes>
           </div>
         </div>
-        <My_Footer new_film={addNewFilm} />
       </div>
     </BrowserRouter>
   );
